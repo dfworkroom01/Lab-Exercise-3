113e1182cb49b3b4390112a813b294b220d1d0da
dfworkroom01 <dfworkroom01@gmail.com>
2024-07-10 13:35:52 +0800

    Add getvalue

---
+++ getValue1.js
@@ -0,0 +1,5 @@
+function getValue1() {
+                    var input = document.getElementById('myInput');
+                    var value = input.value;
+                    console.log('value');
+                    }
