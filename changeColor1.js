d3268942a37e8e60ed275efe1cc16ad8ee873dd4
dfworkroom01 <dfworkroom01@gmail.com>
2024-07-10 13:32:59 +0800

    Maintain the original script for change color

--- changeColor1.js
+++ changeColor1.js
@@ -1,9 +1,7 @@
 function changeColor1() {
                     heading1 = document.getElementById('heading1');
-                    var input = document.getElementById('myInput');
-                    var value = input.value;
-                    console.log('value');
-                        heading1.style.color = "green";
-                            column1 = document.getElementById('column1');
-                            column1.style.background = "white";
+                    heading1.style.color = "green";
+                        column1 = document.getElementById('column1');
+                        column1.style.background = "white";
                     }
+   
