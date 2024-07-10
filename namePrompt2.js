5dc28477a15aac054fcf45b8452475290fb99d1a
dfworkroom01 <dfworkroom01@gmail.com>
2024-07-10 16:01:29 +0800

    Additional JS to change heading2

---
+++ namePrompt2.js
@@ -0,0 +1,12 @@
+function namePrompt2() {
+                let result = window.prompt("What is your nick name?", "Name");
+                let text
+               
+                if (result === null) {
+                   text = "You hit cancel!"
+                } else {
+                   text = result                }
+                let resultDiv = document.getElementById('result');
+                const element = document.getElementById("heading2");
+                element.innerHTML = text
+                }
