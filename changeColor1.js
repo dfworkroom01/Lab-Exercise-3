d51e6fc1f62153b24f521ff7c96edeecee52a5e3
dfworkroom01 <dfworkroom01@gmail.com>
2024-07-10 16:22:15 +0800

    Delete empty lines

--- changeColor1.js
+++ changeColor1.js
@@ -3,5 +3,4 @@ function changeColor1() {
                     heading1.style.color = "green";
                         column1 = document.getElementById('column1');
                         column1.style.background = "white";
-                    }
-   
+                    }   
