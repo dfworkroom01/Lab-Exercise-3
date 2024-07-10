ccaab53941abec1ef5e87a709b86f8ed94b7aac0
dfworkroom01 <dfworkroom01@gmail.com>
2024-07-10 09:57:14 +0800

    JS codes external1

---
+++ changeColor1.js
@@ -0,0 +1,6 @@
+function changeColor1() {
+                    heading1 = document.getElementById('heading1');
+                    heading1.style.color = "green";
+                        column1 = document.getElementById('column1');
+                        column1.style.background = "white";
+                    }
