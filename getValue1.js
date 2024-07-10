3071bbeec7e95a63d469f8bd8ed14bb8557baae0
dfworkroom01 <dfworkroom01@gmail.com>
2024-07-10 15:52:53 +0800

    Change of coding

--- getValue1.js
+++ getValue1.js
@@ -1,5 +1,13 @@
-function getValue1() {
-                    var input = document.getElementById('myInput');
-                    var value = input.value;
-                    console.log('value');
-                    }
+                function namePrompt() {
+                let result = window.prompt("What is your nick name?", "Name");
+                let text
+               
+                if (result === null) {
+                   text = "You hit cancel!"
+                } else {
+                   text = result                }
+                let resultDiv = document.getElementById('result');
+                const element = document.getElementById("heading1");
+                element.innerHTML = text
+                }
+               
