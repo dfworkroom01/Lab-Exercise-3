a68f28e0d767bfceb6fa8d53f6af44c9cd740f9f
dfworkroom01 <dfworkroom01@gmail.com>
2024-07-10 09:57:27 +0800

    JS code external 2

---
+++ changeColor2.js
@@ -0,0 +1,6 @@
+function changeColor2() {
+                    heading2 = document.getElementById('heading2');
+                    heading2.style.color = "Salmon";
+                        column2 = document.getElementById('column2');
+                        column2.style.background = "white";
+                    }
